46185a28deeff25e6d848a2a07d3e7ab47cf7797
Jagadish458 <50491166+Jagadish458@users.noreply.github.com>
2019-06-09 22:50:45 +0530

    Add files via upload

---
+++ HashM.java
@@ -0,0 +1,14 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class HashM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HashMap< String, Integer> h=new HashMap<String, Integer>();
+h.put("ja",100 );
+System.out.println(h);
+	}
+
+}
